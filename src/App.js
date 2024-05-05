1ab29f0f2a05519a54f21c7686215bfe8e04e9c5
Shu Matsumoto <shumatsumoto0328@gmail.com>
2024-05-05 21:50:20 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import openai from "./lib/openai";
+
 function App() {
+  const [content, setContent] = useState("");
+  const [result, setResult] = useState("");
+  const review = async () => {
+    const messages = [{ role: "user", content }];
+    const result = await openai.completion(messages);
+    setResult(result);
+  };
   return (
-    <div className="App">
-      hello, world!
+    <div className="min-h-screen bg-blue-100 flex flex-col items-center justify-center">
+      <header className="flex w-full max-w-5xl justify-between items-center py-4 px-6">
+        <h1 className="text-2xl font-bold text-blue-900">AI Code Reviewer</h1>
+      </header>
+      <main className="flex w-full max-w-5xl bg-white rounded-lg shadow-xl overflow-hidden h-[70vh]">
+        <div className="flex flex-col w-1/2 h-full bg-gray-900 overflow-y-auto">
+          <div className="flex-1 p-4 text-white">
+            <textarea
+              onChange={(e) => {
+                setContent(e.target.value);
+              }}
+              className="h-full w-full bg-transparent text-white resize-none outline-none"
+            />
+          </div>
+          <button
+            onClick={review}
+            className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-md focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
+          >
+            レビューする
+          </button>
+        </div>
+        <div className="flex flex-col w-1/2 h-full items-center justify-center">
+          <div className="p-4 overflow-y-auto w-full">{result}</div>
+        </div>
+      </main>
     </div>
   );
 }
